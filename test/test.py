c3484ee7bc53f3bac548ff4ff02d0405fab093f6
maurizio-martina <40980440+maurizio-martina@users.noreply.github.com>
2024-11-13 23:16:58 +0100

    Update test.py

--- test/test.py
+++ test/test.py
@@ -10,8 +10,8 @@ from cocotb.triggers import ClockCycles
 async def test_project(dut):
     dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, units="us")
+    # Set the clock period to 20 ns (50 MHz)
+    clock = Clock(dut.clk, 20, units="ns")
     cocotb.start_soon(clock.start())
 
     # Reset
@@ -26,15 +26,22 @@ async def test_project(dut):
     dut._log.info("Test project behavior")
 
     # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    # set switches to 0
+    dut.uio_in.value = 0
+    # set notes to 1
+    dut.ui_in.value = 31
+    
+    # Wait for 5 us
+    await ClockCycles(dut.clk, 250)
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
-
+    # move notes
+    dut.ui_in.value = 30
+    await ClockCycles(dut.clk, 1000000)
+    dut.ui_in.value = 31    
+   
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    # assert dut.uo_out.value == 50
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
