690618fbf96726063acdfb511f304f4f1d492625
maurizio-martina <40980440+maurizio-martina@users.noreply.github.com>
2024-11-13 13:35:34 +0100

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,8 @@
 
 `default_nettype none
 
-module synth_simple (
+module tt_um_synth_simple_maurizio-martina (
+//module synth_simple (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
